0fb565c32c1dcbcbd44ad0d9a194c05f25732997
Ijeoma Nwokolo <i.nwokolo@amoejitech.com>
2023-12-21 21:45:00 +0000

    Add Vendor front page

---
+++ packages/frontend/src/vendorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VendorComponent from './VendorComponent';
+
+const VendorPage = () => {
+  return (
+    <div>
+      <h1>Vendor Page</h1>
+      <VendorComponent />
+    </div>
+  );
+};
+
+export default VendorPage;
